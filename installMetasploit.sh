b9f59a2bdae6e249003df2bb5260232623fd9b48
XXXTheInternXXX <me@example.com>
2020-12-28 09:13:23 -0700

    Added nmap to apt-install

--- installMetasploit.sh
+++ installMetasploit.sh
@@ -5,7 +5,7 @@ apt upgrade -y
 apt dist-upgrade -y
 apt autoremove -y
 apt autoclean -y
-apt install postgresql postgresql-contrib -y
+apt install postgresql postgresql-contrib nmap -y
 curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall
 chmod 755 msfinstall
 ./msfinstall
